9c10993b8f8df065cfab0622e8456e8194740566
Teilor Souza Barcelos <tsb-@live.com>
2021-10-22 16:11:15 -0300

    animation with moti added

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,3 +1,4 @@
+import { MotiView } from 'moti'
 import React from 'react'
 
 import {
@@ -25,7 +26,12 @@ interface Props {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -40,6 +46,6 @@ export function Message({ data }: Props) {
           {data.user.name}
         </Text>
       </View>
-    </View>
+    </MotiView>
   )
 }
