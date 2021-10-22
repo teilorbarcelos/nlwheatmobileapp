ec9c958da9abbede1aae9633d9787162f84d1838
Teilor Souza Barcelos <tsb-@live.com>
2021-10-22 10:04:44 -0300

    right to begin to code

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading'
+import { Home } from './src/screens/Home'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    <AppLoading />
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
+  )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {
+  Text,
+  View
+} from 'react-native'
+
+import { styles } from './styles'
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Eu sou o cara!</Text>
+    </View>
+  )
+}
